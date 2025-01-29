7324fb49ac2bdb84d6bc600f7206a8a84cdbbec0
Pedro Paolielo <pedro3997@gmail.com>
2025-01-28 21:08:34 -0300

    Draven Empinando linha na pipa

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do número secreto, meu gato botou um ovo');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
